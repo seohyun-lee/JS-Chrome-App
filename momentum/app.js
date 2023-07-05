39c36c1239ffc78553d8f64285da8fbd52f12037
leeseohyun <applemint2003@gmail.com>
2023-07-05 23:54:27 +0900

    #2.3 const and let

---
+++ momentum/app.js
@@ -0,0 +1,24 @@
+//#2.0
+//alert("hi"); //경고창 띄우기
+
+//#2.1
+console.log(54545454);
+console.log('lala');    //String은 홑따옴표 써도 되고 쌍따옴표 써도 됨
+console.log("lalalala");
+
+//#2.2
+const a = 5; //const로 상수를 생성한다.
+const b = 5;
+const myName = "Gildong";
+
+console.log(a + b);
+console.log(a * b);
+console.log(a / b);
+console.log(myName+" Hong");
+
+//#2.3
+let userName = "user"; //let으로 변수를 생성한다.
+console.log(userName);
+userName = "SH";
+console.log(userName);
+//기본적으로 const를 쓰고, 업데이트가 허용되어야 하는 경우에 let을 쓰자.
